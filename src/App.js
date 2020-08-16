cd89b274520979e2144cc9d3e90b0cec90b7830a
isaiah francois <francoisisaiaht@yahoo.com>
2020-08-16 12:45:28 -0400

    Received news articles from alan and set to react state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
 const App = () => {
+  const [newArticles, setNewsArticles] = useState([]);
+  const alanKey =
+    "02c15c3b2c68587db38042b771d735652e956eca572e1d8b807a3e2338fdd0dc/stage";
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        }
+      },
+    });
+  }, []);
   return (
     <div>
       <h1>Alan AI News Application</h1>
